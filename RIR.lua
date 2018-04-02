1f81da91639d3cb58dee740c0d6b12e91995fb2a
Jelle Meeus <jellemeeus@hotmail.com>
2018-04-03 01:25:01 +0200

    Added whisper listener

--- RIR.lua
+++ RIR.lua
@@ -1,26 +1,18 @@
+local RIR = CreateFrame("Frame")
 local InstanceShortNamesDungeons = {["Utgarde Keep"] = "UK", ["The Nexus"] = "Nexus", ["Azjol-Nerub"] = "AN", ["Ahn'kahet: The Old Kingdom"] = "AK:TOK", ["Drak'Tharon Keep"] = "DTK", ["The Violet Hold"] = "VH", ["Gundrak"] = "GD", ["Halls of Stone"] = "HoS", ["Halls of Lightning"] = "HoL", ["Utgarde Pinnacle"] = "UP", ["The Oculus"] = "Oculus", ["Crusaders' Coliseum: Trial of the Champion"] = "TOC", ["The Culling of Stratholme"] = "CoS", ["Pit of Saron"] = "PoS", ["The Forge of Souls"] = "FoS", ["Halls of Reflection"] = "HoS"}
 local InstanceShortNamesRaids = {["Icecrown Citadel"] = "ICC", ["Vault of Archavon"] = "VOA", ["Trial of the Crusader"] = "TOC", ["Naxxramas"] = "NAXX", ["The Ruby Sanctum"] = "RS", ["The Obsidian Sanctum"] = "OS"}
-
 local RaidDifficulty = {[1] = "10n",[2] = "25n",[3] = "10h",[4] = "25h"}
 
-
-local function MyAddonCommands(msg, editbox)
-  -- pattern matching that skips leading whitespace and whitespace between cmd and args
-  -- any whitespace at end of args is retained
-  local _, _, cmd, args = string.find(msg, "%s?(%w+)%s?(.*)")
+local function ReportLocks(cmd, args, Player)
   local Locks = {}
   local isReportRaid = true
   local isReportHC = true
-   
+  Player = player or UnitName("player")
+
   if cmd == "hc" then
-    --print("hc")
     isReportRaid = false
   elseif cmd == "raid" then
-    --print("raid")
-    isReportHC = false
-    -- Handle removing of the contents of rest... to something.   
-  elseif cmd == "any" then
-    --print("any")       
+    isReportHC = false    
   else
     print("usage: /rir [hc, raid, any] [guild, party, test]")
     do return end  
@@ -36,25 +28,22 @@ local function MyAddonCommands(msg, editbox)
   end
 
   if args == "guild" then
-    --print("to guild") 
     if #Locks > 0 then
       SendChatMessage(string.format("Locks for " .. cmd .. " : %s",table.concat(Locks,", ")),"GUILD")
     else
       SendChatMessage("No locks for " .. cmd, "GUILD", "Common")
     end  
   elseif args == "party" then
-    --print("to party") 
     if #Locks > 0 then
       SendChatMessage(string.format("Locks for " .. cmd .. " : %s",table.concat(Locks,", ")),"PARTY")
     else
       SendChatMessage("No locks for " .. cmd, "PARTY", "Common")
     end 
-  elseif args == "test" then
-    --print("default: to whisper self")   
+  elseif args == "w" or args == "test" then
     if #Locks > 0 then
-        SendChatMessage(string.format("Locks for " .. cmd .. " : %s",table.concat(Locks,", ")), "WHISPER", nil, UnitName("player"))
+        SendChatMessage(string.format("Locks for " .. cmd .. " : %s",table.concat(Locks,", ")), "WHISPER", nil, Player)
     else
-        SendChatMessage("No locks for " .. cmd, "WHISPER", "Common", UnitName("player"))
+        SendChatMessage("No locks for " .. cmd, "WHISPER", "Common", UPlayer)
     end
   else
     print("usage: /rir [hc, raid, any] [guild, party, test]")
@@ -62,5 +51,25 @@ local function MyAddonCommands(msg, editbox)
   end
 end
 
+RIR:RegisterEvent("CHAT_MSG_WHISPER")
+RIR:SetScript("OnEvent", 
+  function(self, event, Message, Player)
+    if event == "CHAT_MSG_WHISPER" then
+      Message = strlower(Message)
+    if Message == "raidlocks" or Message == "raid locks" then
+      ReportLocks("raid", "w", Player)
+    elseif Message == "hclocks" or Message == "hc locks" then
+      ReportLocks("hc", "w", Player)
+    elseif Message == "locks" or Message == "saved" then
+      ReportLocks("any", "w", Player)
+    end
+  end
+end)
+
+local function MyAddonCommands(msg, editbox)
+  local _, _, cmd, args = string.find(msg, "%s?(%w+)%s?(.*)")
+  ReportLocks(cmd, args)  
+end
+
 SLASH_RIR1, SLASH_RIR2 = '/rir', '/locks'
-SlashCmdList["RIR"] = MyAddonCommands   -- add /hiw and /hellow to command list
+SlashCmdList["RIR"] = MyAddonCommands   
